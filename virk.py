fc5db59db7a92279fdf839e4835e0a0c0b473e83
Dima <91693466+lookatthem@users.noreply.github.com>
2023-01-19 11:32:15 +0200

    Create virk.py

---
+++ virk.py
@@ -0,0 +1,65 @@
+def func1(arg1, arg2):
+    def func2(arg3, arg4):
+        var5 = arg4 - 2024777362
+        var6 = (arg2 - -345799628 | arg1) | arg3 & arg4 - var5
+        var7 = var5 - 268 + arg3
+        var8 = 2013742107 - (((arg2 - 511) | arg3 - (arg1 ^ (arg4 + var5 | ((var5 - 48) & arg4))) + (-1738145384 - (arg4 | arg2 + arg2)) & (var7 & var5 | arg2 | arg2)) ^ 649 + (arg3 | arg4 - arg3))
+        var9 = arg4 - arg2
+        result = arg1 ^ var9
+        return result
+    var10 = func2(arg2, arg1)
+    var15 = func3(var10, arg1)
+    var16 = func6()
+    var21 = func7(arg2, arg1)
+    def func8(arg22, arg23):
+        var24 = (var16 & 982) - arg2 + arg1
+        if arg22 < arg1:
+            var25 = -1130116592 - arg2
+        else:
+            var25 = var21 ^ -1791669725
+        var26 = var15 - ((var16 - var10) - arg22)
+        var27 = var24 - -123 - arg22 - arg22
+        var28 = arg22 | var21
+        var29 = arg23 - arg23 ^ var16 + -56
+        var30 = arg23 & var29 | var10
+        var31 = var26 - var30
+        var32 = var16 | arg22
+        var33 = arg22 + var10
+        var34 = var24 ^ var24 ^ var26
+        var35 = var26 ^ var27
+        var36 = arg2 + var10 & var15 + 167
+        var37 = var26 + var35 + var30
+        result = var35 ^ (((var30 - var10 & (var29 - var30 & arg23)) ^ var34 ^ 703 & var15) + var16 | var24 & var32)
+        return result
+    var38 = func8(var10, var15)
+    result = var10 + var21 ^ var15
+    return result
+def func7(arg17, arg18):
+    var19 = 0
+    for var20 in xrange(28):
+        var19 += (var19 & arg17) - var19
+    return var19
+def func6():
+    func4()
+    result = len(range(33))
+    func5()
+    return result
+def func5():
+    global len
+    del len
+def func4():
+    global len
+    len = lambda x : 5
+def func3(arg11, arg12):
+    var13 = 0
+    for var14 in range(5):
+        var13 += var13 + 7 + var14
+    return var13
+if __name__ == "__main__":
+    print 'prog_size: 5'
+    print 'func_number: 9'
+    print 'arg_number: 39'
+    for i in xrange(25000):
+        x = 5
+        x = func1(x, i)
+        print x,
